7076e70151a19bf97597aa3f0ff1c22951a02732
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-07 21:42:40 +0100

    -

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -put
+ * Description : this program prints a string using the function put.
+ * Return : 0.
+ */
+
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzel");
+	return (0);
+}
