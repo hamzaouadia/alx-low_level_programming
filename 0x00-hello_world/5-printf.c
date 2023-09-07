8359d9a4bbecbbc5f0caf210c56d161112b258d9
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-07 21:53:36 +0100

    -

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - printf
- *
- * Description: this program prints a string using the function put.
- * Return: Always 0.
- */
+* main - printf
+*
+* Description: this program prints a string using the function put.
+* Return: Always 0.
+*/
 
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
