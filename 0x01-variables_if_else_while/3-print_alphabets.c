43ea861c0efe94a6966a8451abc3ce0b00a857e4
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-09 02:27:02 +0100

    -

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Description:   a program that prints the
+ *	alphabet in lowercase, and then in uppercase, 
+ *	followed by a new line.
+ * Return: always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		printf("%c", c);
+		c++;
+	}
+	c = 'A';
+    while (c <= 'Z')
+	{
+		printf("%c", c);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
