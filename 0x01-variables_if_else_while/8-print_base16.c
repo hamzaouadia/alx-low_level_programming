1e7b54677cf852ec9d143751eb65e265b65d08a2
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-09 03:16:35 +0100

    -

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - putchar
+ *
+ * Description: program that prints all single digit
+ *	numbers of base 10 starting from 0,
+ *	followed by a new line.
+ * Return: always 0.
+ */
+int main(void)
+{
+	int c;
+
+	c = 48;
+	while (c <= 57)
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
