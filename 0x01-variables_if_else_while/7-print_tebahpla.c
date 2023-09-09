423302963409cce2543ee6384979f4be9239304e
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-09 03:11:46 +0100

    -

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - putchar
+ *
+ * Description: program that prints the lowercase alphabet in reverse
+ *	followed by a new line.
+ * Return: always 0.
+ */
+int main(void)
+{
+	char	*str;
+	int 	c;
+
+	str[8] = "ebahpla";
+	c = 7;
+	while (c <= 0)
+	{
+		putchar(str[c]);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
