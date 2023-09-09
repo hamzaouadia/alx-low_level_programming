4b9508df84637ef56e8f40c8d8ac878dea6e37a2
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-09 03:21:44 +0100

    -

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - putchar
+ *
+ * Description:  program that prints all possible combinations of single-digit numbers.
+ *	followed by a new line.
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 9)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
