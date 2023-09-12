d4ac657812f3147531e67190c909c6aca1a3e2ed
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-12 23:17:55 +0100

    -

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  * jack_bauer - read description
  *
  * Description: function that prints every minute of the day of Jack Bauer,
- * 	starting from 00:00 to 23:59.
+ *	starting from 00:00 to 23:59.
  * Return: nothing.
  */
 void    jack_bauer(void)
