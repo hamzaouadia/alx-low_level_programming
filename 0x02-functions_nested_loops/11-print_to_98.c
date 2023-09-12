0b44368f2dd29e0eabf8be6ea9e3c682d2f30d1e
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-13 00:08:34 +0100

    -

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void	print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d", n++);
-			if (n != 98)
+			if (n - 1 != 98)
 				printf(", ");
 		}
 	}
@@ -23,7 +23,7 @@ void	print_to_98(int n)
 		while (n >= 98)
 		{
 			printf("%d", n--);
-			if (n != 98)
+			if (n + 1 != 98)
 				printf(", ");
 		}
 	}
