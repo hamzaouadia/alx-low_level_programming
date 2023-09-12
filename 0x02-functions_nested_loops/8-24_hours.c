5b62bc0843b1b5ca81684e054f18407cf21748cd
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-12 23:17:34 +0100

    -

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,6 @@ void    jack_bauer(void)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-			_putchar((i / 10) + '0');
 			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
