3a1ca2ee0a8c65fe9919b5dd679609a39f4fe078
Hamza Ouadia <haouadia@c3r5p9.1337.ma>
2023-09-18 14:01:51 +0100

    -

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 
 /**
  *	resete_to_98 - read description
+ *
  *	@n: a pointer to an integer.
  *	Description: function that takes a pointer to an int
  *		as parameter and updates the value it points to to 98.
