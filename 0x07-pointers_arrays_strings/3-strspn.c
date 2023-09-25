8237550a994470a5a4b2ff56cdbd678db9a80276
Hamza Ouadia <haouadia@c3r1p6.1337.ma>
2023-09-26 00:51:10 +0100

    -

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *	_strspn - function that locates a character in a string.
+ *	@s: string.
+ *	@accept: string.
+ *	Return: string or NULL,
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int k;
+
+	while (s[i])
+	{
+		k = 0;
+		while (accept[k])
+		{
+			if (s[i] == accept[k])
+			{
+				j++;
+				break;
+			}
+			k++;
+		}
+		if (!accept[k])
+			break;
+		i++;
+	}
+	return (j);
+}
