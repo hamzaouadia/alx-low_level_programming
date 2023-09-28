8bcd3c8465b16f62a6b523205712308539fc911c
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-29 00:49:43 +0100

    -

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *	print_diagsums -  function that prints a string, followed by a new line.
+ *	_puts_recursion -  function that prints a string, followed by a new line.
  *	@s: string.
  *	Return: void.
 */
