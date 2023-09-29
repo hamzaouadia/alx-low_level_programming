b67bb9028e2f07b1045451f0286d43c568eb84c8
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-09-29 01:15:56 +0100

    -

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *	_sqrt - a function that checks for square root of number.
+ *	@n: number.
+ *	@n: number.
+ *	Return: intiger.
+*/
+
+int _sqrt(int n, int r)
+{
+	if (r == n / r)
+		return (r);
+	if (r == n / 2)
+		return (-1);
+	return (_sqrt(n, r + 1));
+}
+
+/**
+ *	_sqrt_recursion - a function that returns the value
+ *		of x raised to the power of y.
+ *	@n: number.
+ *	Return: intiger.
+*/
+
+int _sqrt_recursion(int n)
+{
+	int r = 0;
+
+	if (n == 2)
+		return (1);
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, r));
+}
