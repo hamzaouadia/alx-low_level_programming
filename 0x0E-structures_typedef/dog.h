7362e73773342147ffb69df4cad708a6d8b0fb4c
hamzaouadia <hamza.ouadia2023@gmail.com>
2023-10-11 03:12:44 +0100

    -

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdlib.h>
 
 /**
  *	struct dog - all dog info.
@@ -15,5 +16,7 @@ struct dog
 	char *owner;
 	float age;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
